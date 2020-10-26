eca2a679f62b8b40841add9732e9ae79b3a9badb
siying <siyinghan@foxmail.com>
2020-10-26 22:38:39 +0100

    update

--- java/java-test/src/Main.java
+++ java/java-test/src/Main.java
@@ -1,15 +1,11 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 10;
-        int temp = i;
-        i++;
-        i = temp;
-        System.out.println(i);
     }
 }
