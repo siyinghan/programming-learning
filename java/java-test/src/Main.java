ffdf4929c89e39413a4b78f53457c476311bf50c
siying <siyinghan@foxmail.com>
2020-11-06 22:36:59 +0100

    update

--- java/java-test/src/Main.java
+++ java/java-test/src/Main.java
@@ -1,6 +1,9 @@
 import java.time.LocalDate;
 import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
