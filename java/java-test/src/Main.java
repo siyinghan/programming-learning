7b747cf6ba09d6465060ae78d3ea2ea75b8e991a
siying <siyinghan@foxmail.com>
2021-02-02 21:32:15 +0100

    update

--- java/java-test/src/Main.java
+++ java/java-test/src/Main.java
@@ -1,18 +1,12 @@
-import javax.swing.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.rmi.registry.Registry;
-import java.text.DecimalFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.text.*;
 import java.time.LocalDate;
 import java.util.*;
-import java.util.function.*;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
