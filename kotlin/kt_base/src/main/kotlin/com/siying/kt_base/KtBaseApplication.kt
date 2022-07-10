7226f99acf2b7d5e8fe095782dcf1301dd394971
siying <siyinghan@foxmail.com>
2022-07-10 11:14:33 +0200

    update

---
+++ kotlin/kt_base/src/main/kotlin/com/siying/kt_base/KtBaseApplication.kt
@@ -0,0 +1,11 @@
+package com.siying.kt_base
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtBaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtBaseApplication>(*args)
+}
