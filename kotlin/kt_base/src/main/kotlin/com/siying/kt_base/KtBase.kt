dccc2e70a1ab8b2987fece7b2e84d331d5bc36f6
siying <siyinghan@foxmail.com>
2023-08-05 23:16:39 +0200

    update

--- kotlin/kt_base/src/main/kotlin/com/siying/kt_base/KtBase.kt
+++ kotlin/kt_base/src/main/kotlin/com/siying/kt_base/KtBase.kt
@@ -1,2 +1,23 @@
 package com.siying.kt_base
 
+fun main() {
+}
+
+data class Hero(
+    val name: String,
+    val age: Int,
+    val gender: Gender?
+)
+
+enum class Gender {
+    MALE, FEMALE
+}
+
+val heroes = listOf(
+    Hero("The Captain", 60, Gender.MALE),
+    Hero("Frenchy", 42, Gender.MALE),
+    Hero("The Kid", 9, null),
+    Hero("Lady Lauren", 29, Gender.FEMALE),
+    Hero("First Mate", 29, Gender.MALE),
+    Hero("Sir Stephen", 37, Gender.MALE)
+)
